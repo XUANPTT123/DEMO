3903742846063ef4ccd95d1a3f070821b656227e
PW - Thanh Xuan <xuanthanhpham8119@gmail.com>
2025-08-16 18:49:36 +0700

    //thay doi text

---
+++ hello.js
@@ -0,0 +1,46 @@
+// let name = 'name'
+// console.log("My name is:" + name)
+
+// name = "XUAN"
+// console.log("My name is:" + name)
+
+// name = 100
+// console.log("My name is:" + name)
+
+// const className = "hangso"
+// console.log(className)
+
+// className = "hangso1"
+
+// let name = "Xuan"
+// let boo = true
+// let a123 = 200;
+
+// console.log(name)
+// console.log(boo)
+// console.log(a123)
+
+// const a  = 5
+// const b = 5
+
+// console.log(a!==b)
+
+// let a = 10;
+// a--;
+// console.log(a--);
+
+
+// let thue = 0;
+// const thunhap = 10000;
+
+// if(thunhap>50){
+//     thue = "20%";
+// }
+// if (thunhap<50){
+//     thue = "10%";
+// }
+// console.log(thue);
+
+for (let i = 0; i < 1000; i++) {
+    console.log("Hello");
+}
